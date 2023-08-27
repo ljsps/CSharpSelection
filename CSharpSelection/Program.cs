97e8eb878f57879b3e23d3542edfd0b2aea7d614
TechDomani <73493660+TechDomani@users.noreply.github.com>
2023-08-27 22:41:47 +0100

    Add project files.

---
+++ CSharpSelection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace CSharpSelection
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("# Assign Letter Grade");
+			string grade = SelectionExamples.AssignLetterGrade(90);
+			Console.WriteLine($"Your grade was a {grade}\n");
+			// Can use Debug.Assert to check return values
+			Debug.Assert(SelectionExamples.AssignLetterGrade(70) == "Merit", "Merit expected");
+
+			Console.WriteLine("# Conditional Operations");
+			SelectionExamples.ConditionalOperations(false, true);
+			Console.WriteLine();
+
+			Console.WriteLine("Check for valid Input");
+			InputNumber();
+			Console.WriteLine();
+
+			Console.WriteLine("# Lift Takeoff");
+			string message = SelectionExamples.LiftTakeoff(5, 142d);
+			Console.WriteLine($"{message}\n");
+
+			Console.WriteLine("# Determine tip");
+			decimal tip = SelectionExamples.DetermineTip("Good");
+			Console.WriteLine($"The tip will be {tip}\n");
+
+			Console.WriteLine("# Determine tip with enum");
+			decimal tip2 = SelectionExamples.DetermineTip(SelectionExamples.ServiceLevel.Good);
+			Console.WriteLine($"The tip will be {tip2}\n");
+		}
+
+		static void InputNumber()
+		{
+			Console.WriteLine("Please enter a number between one and fifty");
+			string? strNum = Console.ReadLine();
+			if (int.TryParse(strNum, out int val))
+			{
+				if (val > 0 && val < 51)
+				{
+					Console.WriteLine("Your number is valid");
+				}
+				else
+				{
+					Console.WriteLine("Your number must be between one and fifty");
+				}
+			}
+			else
+			{
+				Console.WriteLine("You did not enter a valid number");
+			}			
+		}
+	}
+}
---
+++ CSharpSelection/SelectionExamples.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSelection
+{
+	public static class SelectionExamples
+	{
+		public static void ConditionalOperations(bool hasBrothers, bool hasSisters)
+		{
+			// Conditional OR
+			if (hasBrothers || hasSisters)
+			{
+				Console.WriteLine("You have siblings");
+			}
+			// Conditional AND
+			if (hasBrothers && hasSisters)
+			{
+				Console.WriteLine("You have both brothers and sisters");
+			}
+			// Conditional XOR
+			if (hasBrothers | hasSisters)
+			{
+				Console.WriteLine("You have brothers or sisters but not both");
+			}
+			// Conditional NOT
+			if (!hasBrothers)
+			{
+				Console.WriteLine("You do not have brothers");
+			}
+			if (!hasSisters)
+			{
+				Console.WriteLine("You do not have sisters");
+			}
+			if (!hasBrothers && !hasSisters)
+			{
+				Console.WriteLine("You are an only child");
+			}
+
+		}
+		public static string AssignLetterGrade(int mark)
+		{
+			if (mark < 0 || mark > 100)
+			{
+				throw new ArgumentOutOfRangeException($"Mark should be between 0 and 100");
+			}
+			string grade;
+			if (mark > 80)
+			{
+				grade = "Distinction";
+			}
+			else if (mark >= 70)
+			{
+				grade = "Merit";
+			}
+			else if (mark >= 60)
+			{
+				grade = "Pass";
+			}
+			else
+			{
+				grade = "Fail";
+			}
+			return grade;
+		}
+
+		public static string LiftTakeoff(int numberOfPassengers, double averageWeightInPounds)
+		{
+			// Use of constants
+			const double PoundsToKg = 0.454d;
+			const int LiftCapacity = 600;
+
+			double weightInKg = averageWeightInPounds * PoundsToKg;
+			double totalWeight = weightInKg * numberOfPassengers;
+
+			// Ternary operator
+			string message = totalWeight < LiftCapacity ? "Lift good to go" : "Too many people in lift";
+			return message;
+		}
+
+		public static decimal DetermineTip(string service)
+		{
+			decimal tip;
+			service = service.ToLower();
+			switch (service)
+			{
+				case "excellent":
+					tip = 0.2m;
+					break;
+				case "good":
+					tip = 0.15m;
+					break;
+				case "ok":
+					tip = 0.1m;
+					break;
+				default:
+					tip = 0m;
+					break;
+			}
+			return tip;
+		}
+
+		public enum ServiceLevel
+		{
+			Excellent,
+			Good,
+			Ok,
+			Rubbish
+		}
+		public static decimal DetermineTip(ServiceLevel service)
+		{
+			decimal tip;
+			switch (service)
+			{
+				case ServiceLevel.Excellent:
+					tip = 0.2m;
+					break;
+				case ServiceLevel.Good:
+					tip = 0.15m;
+					break;
+				case ServiceLevel.Ok:
+					tip = 0.1m;
+					break;
+				default:
+					tip = 0m;
+					break;
+			}
+			return tip;
+		}
+
+	}
+}
